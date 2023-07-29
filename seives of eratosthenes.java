3d43208904bad1c8595a1f31fb42085973ad90d1
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-07-29 06:13:08 +0530

    print all prime numbers upto a given range - sieves of eratosthenes algorithms

---
+++ seives of eratosthenes.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int n = scn.nextInt();
+        System.out.println();
+        
+        System.out.println("==================================");
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        
+        for(int i = 2; i * i <= isPrime.length; i++) {
+            if(isPrime[i] == true) {
+                for(int j = i + i; j < isPrime.length; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        
+        // print elements
+        for(int i = 2; i < isPrime.length; i++) {
+            if(isPrime[i] == true) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
